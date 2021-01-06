daa0c44a2c756651d69780a7b4ccfadc11319a09
ducquang2 <quangbilun@gmail.com>
2021-01-06 21:00:32 +0700

    add index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // this changed
+// import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
