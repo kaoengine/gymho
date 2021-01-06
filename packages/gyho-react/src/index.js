88306af2765b458cc987129aec918775c4a85462
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2021-01-06 21:09:35 +0700

    Update index.js

--- packages/gyho-react/src/index.js
+++ packages/gyho-react/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // this changed
-// import * as serviceWorker from './serviceWorker';
-
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
