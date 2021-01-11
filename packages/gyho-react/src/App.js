f44d9eb5d7fb4ee96124c4598f4312a64867990a
Tran <qtran31@dxc.com>
2021-01-11 15:00:02 +0700

    card list

--- packages/gyho-react/src/App.js
+++ packages/gyho-react/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Carddetail from "./components/Carddetail"
+import CardList from "./components/CardList"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
     render() {
         return (
-            <Carddetail></Carddetail>
+            <CardList></CardList>
         )
     }
 }
