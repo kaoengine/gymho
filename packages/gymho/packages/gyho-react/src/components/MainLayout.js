17fdc9debe9873e7fa9b6964a0ffc8673bd41f09
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2021-01-12 15:04:26 +0700

    Merge pull request #16 from kaoengine/layout-implement
    Layout implement

---
+++ packages/gymho/packages/gyho-react/src/components/MainLayout.js
@@ -0,0 +1,200 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable react/no-multi-comp */
+
+import { createMedia } from '@artsy/fresnel'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
+
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    mobile: 0,
+    tablet: 768,
+    computer: 1024,
+  },
+})
+
+/* Heads up!
+ * Layout uses inline styling, however it's not the best practice. Use CSS or styled
+ * components for such things.
+ */
+const Layout = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='header'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+  </Container>
+)
+
+Layout.propTypes = {
+  mobile: PropTypes.bool,
+}
+
+/* Heads up!
+ * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
+ * It can be more complicated, but you can create really flexible markup.
+ */
+class DesktopContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { children } = this.props
+    const { fixed } = this.state
+
+    return (
+      <Media greaterThan='mobile'>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em' }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>
+                  Home
+                </Menu.Item>
+                <Menu.Item as='a'>Work</Menu.Item>
+                <Menu.Item as='a'>Company</Menu.Item>
+                <Menu.Item as='a'>Careers</Menu.Item>
+              </Container>
+            </Menu>
+            <Layout />
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Media>
+    )
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+  state = {}
+
+  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+
+  handleToggle = () => this.setState({ sidebarOpened: true })
+
+  render() {
+    const { children } = this.props
+    const { sidebarOpened } = this.state
+
+    return (
+      <Media as={Sidebar.Pushable} at='mobile'>
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={sidebarOpened}
+          >
+            <Menu.Item as='a' active>
+              Pyho
+            </Menu.Item>
+            <Menu.Item as='a'>News</Menu.Item>
+            <Menu.Item as='a'>Activity</Menu.Item>
+            <Menu.Item as='a'>Goal</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Segment
+              inverted
+              textAlign='center'
+              style={{ minHeight: 150, padding: '1em 0em' }}
+              vertical
+            >
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <Layout mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Media>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+  /* Heads up!
+   * For large applications it may not be best option to put all page into these containers at
+   * they will be rendered twice for SSR.
+   */
+  <MediaContextProvider>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </MediaContextProvider>
+)
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const MainLayout = () => (
+  <ResponsiveContainer>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column floated='right' width={6}>
+            <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+  </ResponsiveContainer>
+)
+
+export default MainLayout
