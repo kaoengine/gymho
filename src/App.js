f31a959e75397b8b9388b4b990e6ef331794479f
ducquang2 <quangbilun@gmail.com>
2021-01-12 12:05:45 +0700

    timer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import CardDetail from "./components/Carddetail";
 import CardList from "./components/CardList"
 import 'semantic-ui-css/semantic.min.css'
 
 class App extends React.Component {
     render() {
         return (
-            <CardList></CardList>
+            <CardDetail></CardDetail>
         )
     }
 }
