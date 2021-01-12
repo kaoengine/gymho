e8b29471599285897db5e6b2484cfbbc2dd0831b
ducquang2 <quangbilun@gmail.com>
2021-01-12 15:04:54 +0700

    update

--- src/components/Carddetail.js
+++ src/components/Carddetail.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import { Button, Header, Container, Image } from 'semantic-ui-react';
+import { Button, Header, Container, Image, Segment } from 'semantic-ui-react';
 import Countdown from "./Countdown";
 
 export default class CardDetail extends Component {
     render() {
         return (
             <Container textAlign="center">
-                <Container>
-                    <Button circular floated="left" icon='arrow left' />
-                    <Button circular floated="right" icon="setting" />
-                    <Header>Video's title</Header>
-                </Container>
-                <Container>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped size="medium" />
-                </Container>
-                <Container>
-                    <Countdown />
-                </Container>
+                <Segment.Group>
+                    <Segment>
+                        <Button circular floated="left" icon='arrow left' />
+                        <Button circular floated="right" icon="setting" />
+                        <Header>Video's title</Header>
+                    </Segment>
+                    <Segment>
+                        <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped size="medium" />
+                    </Segment>
+                    <Segment>
+                        <Countdown />
+                    </Segment>
+                </Segment.Group>
             </Container>
         );
     }
