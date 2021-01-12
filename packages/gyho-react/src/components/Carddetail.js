7021dba4882f90da23d8be4f70b365b34f00906e
Đức Quang <70498757+ducquang2@users.noreply.github.com>
2021-01-12 15:06:37 +0700

    Merge pull request #17 from kaoengine/card-detail
    Card detail

--- packages/gyho-react/src/components/Carddetail.js
+++ packages/gyho-react/src/components/Carddetail.js
@@ -1,54 +1,25 @@
 import React, { Component } from "react";
-import { Button, Card, Icon, Image } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css'
-
+import { Button, Header, Container, Image, Segment } from 'semantic-ui-react';
+import Countdown from "./Countdown";
 
 export default class CardDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
-
-    // change code below this line
-    increment() {
-        this.setState({
-            count: this.state.count + 1
-        });
-    };
-
-    decrement() {
-        this.setState({
-            count: this.state.count - 1
-        });
-    };
-
-    reset() {
-        this.setState({
-            count: 0
-        });
-    };
-
-
     render() {
         return (
-            <Card>
-                <Card.Content>
-                    <Button circular floated='left' icon='x' />
-                    <Card.Header textAlign={"right"}>Video's title</Card.Header>
-                </Card.Content>
-                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-                <Card.Content textAlign={"center"}>
-                    <Card.Description>
-                        Current Count: {this.state.count}
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Button color='red' floated="left" icon='minus' onClick={(e) => this.decrement(e)} />
-                    <Button color='green' floated="right" icon='plus' onClick={(e) => this.increment(e)} />
-                </Card.Content>
-            </Card>
+            <Container textAlign="center">
+                <Segment.Group>
+                    <Segment>
+                        <Button circular floated="left" icon='arrow left' />
+                        <Button circular floated="right" icon="setting" />
+                        <Header>Video's title</Header>
+                    </Segment>
+                    <Segment>
+                        <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped size="medium" />
+                    </Segment>
+                    <Segment>
+                        <Countdown />
+                    </Segment>
+                </Segment.Group>
+            </Container>
         );
     }
 }
