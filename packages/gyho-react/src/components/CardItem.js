f6245fe6b7d1e6d3c6790022963e6bd53197f21f
Tran <qtran31@dxc.com>
2021-01-21 14:16:27 +0700

    update card list

--- packages/gyho-react/src/components/CardItem.js
+++ packages/gyho-react/src/components/CardItem.js
@@ -1,30 +1,22 @@
 import React, { Component } from "react";
-import { Button, Icon, Image, Item, Label } from 'semantic-ui-react'
-
-
-
-
+import { Button, Icon, Image, Item, Label } from "semantic-ui-react";
+import './card-item.css'
 export default class CardItem extends Component {
   constructor(props) {
     super(props);
-}
+  }
   render() {
     return (
-        <Item>
-          <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+      <Item>
+        <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
 
-          <Item.Content>
-    <Item.Header as="a">Dumbell {this.props.name}</Item.Header>
-            <Item.Meta>
-              <span className="cinema">IFC</span>
-            </Item.Meta>
-            <Item.Description>KaoItems</Item.Description>
-            <Button primary floated="right">
-              Start
-              <Icon name="right chevron" />
-            </Button>
-          </Item.Content>
-        </Item>
+        <Item.Content>
+          <Item.Header as="a">Dumbell {this.props.name}</Item.Header>
+          <Button style={{ height: "50%" }} circular primary floated="right">
+            Start
+          </Button>
+        </Item.Content>
+      </Item>
     );
   }
 }
