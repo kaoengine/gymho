42bfb1784c1a9ff191163b2cc87f2d9eb7b7f56a
Tran Khanh <16370701+trkhanh@users.noreply.github.com>
2021-01-30 08:00:32 +0700

    Merge pull request #26 from kaoengine/kdev291
    implement counter on Component with mockdata

--- packages/gyho-react/src/pages/Activity.jsx
+++ packages/gyho-react/src/pages/Activity.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import ALayout from '../Layouts/ALayout'
 import ExerciseList from '../components/ExerciseList'
+import { useLocalStorage } from '../hooks'
+
+
 
 export default function ActivityPage() {
+    const defaultValue = [
+        {
+            "id": "pull",
+            "counter": 1
+        },
+        {
+            "id": "push",
+            "counter": 1
+        },
+        {
+            "id": "arm",
+            "counter": 1
+        },
+        {
+            "id": "squat",
+            "counter": 1
+        },
+        {
+            "id": "scrun",
+            "counter": 1
+        },
+        {
+            "id": "shoulder",
+            "counter": 1
+        },
+    ]
+    const [data, setValue] = useLocalStorage('data', defaultValue);
+    const updateExercies = (item) => {
+        if (!data) {
+            setValue(defaultValue)
+        }
+        setValue(data.map(x => x.id === item ? { ...x, counter: x.counter + 1 } : x))
+    }
+
     return (
-        <ALayout main={<ExerciseList></ExerciseList>}></ ALayout>
+        <ALayout main={<ExerciseList updateExercies={updateExercies} data={data}></ExerciseList>}></ ALayout>
     );
 }
