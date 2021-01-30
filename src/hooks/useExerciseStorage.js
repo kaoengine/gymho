d22fa161bfb5bbf66c7f05a775afdfee0dd05c72
Khanh <tran_khanh@outlook.com>
2021-01-30 12:27:48 +0700

    quick-implement data for demo

--- src/hooks/useExerciseStorage.js
+++ src/hooks/useExerciseStorage.js
@@ -3,44 +3,107 @@ import useLocalStorage from './useLocalStorage';
 export default function useExerciseStorage(key) {
     const defaultValue = [
         {
-            "id": "pull",
-            "counter": 1
+            "date": '1/28/2021',
+            "content": [{
+                "id": "pull",
+                "counter": 1
+            },
+            {
+                "id": "push",
+                "counter": 1
+            },
+            {
+                "id": "arm",
+                "counter": 1
+            },
+            {
+                "id": "squat",
+                "counter": 1
+            },
+            {
+                "id": "scrun",
+                "counter": 1
+            },
+            {
+                "id": "shoulder",
+                "counter": 1
+            }]
         },
         {
-            "id": "push",
-            "counter": 1
-        },
-        {
-            "id": "arm",
-            "counter": 1
-        },
-        {
-            "id": "squat",
-            "counter": 1
-        },
-        {
-            "id": "scrun",
-            "counter": 1
-        },
-        {
-            "id": "shoulder",
-            "counter": 1
-        },
-    ]
-    const [data, setToLocalStorage = setValue] = useLocalStorage(key, defaultValue);
+            "date": '1/29/2021',
+            "content": [{
+                "id": "pull",
+                "counter": 1
+            },
+            {
+                "id": "push",
+                "counter": 1
+            },
+            {
+                "id": "arm",
+                "counter": 1
+            },
+            {
+                "id": "squat",
+                "counter": 1
+            },
+            {
+                "id": "scrun",
+                "counter": 1
+            },
+            {
+                "id": "shoulder",
+                "counter": 1
+            }]
+        }]
 
+    const [data, setToLocalStorage = setValue] = useLocalStorage(key, defaultValue);
     const setExecercise = (exercise) => {
         try {
-            console.log(exercise)
             // Set to localstorage
-            
-            const newData = data.map(x => x.id === exercise ? { ...x, counter: x.counter + 1 } : x)
-            setToLocalStorage(newData);
+            const currentDate = new Date();
+            const yesterdayDate = currentDate.setDate(currentDate.getDate() - 1);
+            const targetDate = data.filter(x => new Date(x.date) > yesterdayDate)
+            let exercies = targetDate[targetDate.length - 1]
+            if (!exercies) {
+                exercies = {
+                    "date": new Date(),
+                    "content": [{
+                        "id": "pull",
+                        "counter": 1
+                    },
+                    {
+                        "id": "push",
+                        "counter": 1
+                    },
+                    {
+                        "id": "arm",
+                        "counter": 1
+                    },
+                    {
+                        "id": "squat",
+                        "counter": 1
+                    },
+                    {
+                        "id": "scrun",
+                        "counter": 1
+                    },
+                    {
+                        "id": "shoulder",
+                        "counter": 1
+                    }]
+                }
+                data.push(exercies)
+            };
+
+            exercies.content = exercies.content.map(x => x.id === exercise ? { ...x, counter: x.counter + 1 } : x)
+            const newData = data.map(x => x.date == exercies.date ? { ...x, content: exercies.content } : x)
+            setToLocalStorage([...newData]);
         } catch (e) {
             console.log(e);
         }
     };
 
-    const exercises = data
+    const exercises = data[0].content
     return [exercises, setExecercise];
 }
