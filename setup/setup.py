979f8b47ca9f7e6343f15c35e2e36cc71dea7747
Khanh <tran_khanh@outlook.com>
2021-02-04 08:51:21 +0700

    update setup for darwin

--- setup/setup.py
+++ setup/setup.py
@@ -1,11 +1,13 @@
 
-import subprocess,sys
+import subprocess
+import sys
 
-if  sys.platform.startswith('linux'):
+if sys.platform.startswith('linux'):
     # Linux-specific code here...
     p = subprocess.Popen(['sh', './install.sh'], stdout=sys.stdout)
 elif sys.platform.startswith('win32'):
     # Win-specific code here...
-    p = subprocess.Popen(['powershell.exe', './install.ps1'], stdout=sys.stdout)
+    p = subprocess.Popen(
+        ['powershell.exe', './install.ps1'], stdout=sys.stdout)
 elif sys.platform.startswith('darwin'):
-    print(3)
+    p = subprocess.Popen(['sh', './install.sh'], stdout=sys.stdout)
