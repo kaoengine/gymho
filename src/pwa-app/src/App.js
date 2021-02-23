33305f36dccb6fdbe082bfd1811c4f5cce875e85
Khanh <tran_khanh@outlook.com>
2021-02-23 18:25:14 +0700

    add route for app

--- src/pwa-app/src/App.js
+++ src/pwa-app/src/App.js
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 import ActivityPage from "./pages/Activity"
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import SurveyPage from "./pages/Survey"
-import { Fragment } from "react";
+
+import {
+    BrowserRouter as Router
+    ,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,14 +20,47 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
-        const classes = useStyles();
+    const classes = useStyles();
+    return (
 
-        return (
-            <div className={classes.root}>
-                {/* <ActivityPage></ActivityPage> */}
-                <SurveyPage></SurveyPage>
+        // <div className={classes.root}>
+        //     <ActivityPage></ActivityPage>
+        //     {/* <SurveyPage></SurveyPage> */}
+        // </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/activity">activity</Link>
+                        </li>
+                        <li>
+                            <Link to="/process">process</Link>
+                        </li>
+                        <li>
+                            <Link to="/survey">Survey</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/activity">
+                        <ActivityPage />
+                    </Route>
+                    <Route path="/survey">
+                        <SurveyPage />
+                    </Route>
+                    <Route path="/process">
+                        Hello
+                    </Route>
+                    <Route path="/">
+                        <ActivityPage />
+                    </Route>
+                </Switch>
             </div>
-        )
-}
+        </Router>
+    );
 
-// export default App;
+}
