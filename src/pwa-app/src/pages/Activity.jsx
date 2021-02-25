d5851f3c5cab98a7e9d600d5d23d1a56ea4144ce
quantran <trtquan.com>
2021-02-25 17:42:12 +0700

    update

--- src/pwa-app/src/pages/Activity.jsx
+++ src/pwa-app/src/pages/Activity.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import ALayout from '../Layouts/ALayout'
-import ExerciseList from '../components/ExerciseList'
-import { useExerciseStorage } from '../hooks'
-
-
+import ALayout from '../Layouts/ALayout';
+import ExerciseList from '../components/ExerciseList';
+import { useExerciseStorage } from '../hooks';
 
 export default function ActivityPage() {
-    const [exercises, setExecercise] = useExerciseStorage('data');
-    const updateExercies = (exercise) => {
-        if (exercise) {
-            setExecercise(exercise)
-        }
+  const [exercises, setExecercise] = useExerciseStorage('data');
+  const updateExercies = exercise => {
+    if (exercise) {
+      setExecercise(exercise);
     }
+  };
 
-    return (
-        <ALayout main={<ExerciseList updateExercies={updateExercies} data={exercises}></ExerciseList>}></ ALayout>
-    );
+  return (
+    <ALayout
+      main={
+        <ExerciseList
+          updateExercies={updateExercies}
+          data={exercises}
+        ></ExerciseList>
+      }
+    ></ALayout>
+  );
 }
